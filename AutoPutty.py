1f2396dbe82b891c79d6602ed3a09ab06106dd4f
SasounTorossian <46853885+SasounTorossian@users.noreply.github.com>
2020-01-17 21:55:15 +0000

    Organised images in to work and home. images and file path combine in new list comprehension.

--- AutoPutty.py
+++ AutoPutty.py
@@ -3,18 +3,29 @@ import pyautogui
 import time
 import psutil
 
-# NOTE: When taken to new monitor/laptop, need to retake pictures
+# Baud Rate and com port to communicate with nrf52840 Device via USB
+BAUD_RATE = '115200'
+COM_PORT = 'COM10'  # Not guaranteed (Prompt user for COM port number?)
 
-BAUD_RATE = '115200'                        # Required baud rate to communicate with device
-COM_PORT = 'COM10'                          # Expected com port device will appear on. Not guaranteed (Prompt user for COM port number?)
-PUTTY_WINDOW_X_Y = (620, 200, 675, 675)     # (x, y, width, height), used in "region" to narrow down search area.
-IMAGE_SEQUENCE = ('Keyboard.PNG'
-                  , 'The Backspace key.PNG'
-                  , 'The Function keys and keypad.PNG'
-                  , 'Session.PNG'
-                  , 'Connection type.PNG'
-                  , 'Speed.PNG'
-                  , 'Serial line.PNG')      # Sequence of image to search in putty
+# folder position to append depending on development on home computer or work computer
+WORK_FOLDER = 'work images/'
+HOME_FOLDER = 'home images/'
+
+# (x, y, width, height), used in "region" to narrow down search area.
+PUTTY_WINDOW_WORK = (620, 200, 675, 675)
+PUTTY_WINDOW_HOME = (660, 265, 600, 540)
+
+# Sequence of image to search in putty
+BASE_IMAGE_SEQUENCE = ['Keyboard.PNG'
+                        , 'The Backspace key.PNG'
+                        , 'The Function keys and keypad.PNG'
+                        , 'Session.PNG'
+                        , 'Connection type.PNG'
+                        , 'Speed.PNG'
+                        , 'Serial line.PNG']
+
+# Create final image list with correct file path pre-appended
+file_image_sequence = [(HOME_FOLDER + image) for image in BASE_IMAGE_SEQUENCE]
 
 
 # Converts strings to array of characters
@@ -23,7 +34,8 @@ def word_to_list(word):
 
 
 # Much better error handling here. Don't want to accidentally spawn too many instances
-def check_and_kill_process(process_name):
+# Searches for and kills any existing putty program
+def check_kill_open_putty(process_name):
     for processes in psutil.process_iter():
         try:
             # Check if process name contains the given name string.
@@ -31,32 +43,30 @@ def check_and_kill_process(process_name):
                 processes.kill()
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
+    subprocess.Popen([r"C:\Program Files\PuTTY\putty.exe"])
+    time.sleep(0.5)
 
 
-'''Open Putty'''
-# Searches for and kills any existing putty program
-check_and_kill_process("Putty")
-subprocess.Popen([r"C:\Program Files\PuTTY\putty.exe"])
-time.sleep(0.5)
+check_kill_open_putty("Putty")
 
 '''Configure putty'''
 # error handling
 s = time.time()
 
-for images in IMAGE_SEQUENCE:
+# put in func
+for images in file_image_sequence:
     try:
-        x, y = pyautogui.locateCenterOnScreen(images, region=PUTTY_WINDOW_X_Y)
+        x, y = pyautogui.locateCenterOnScreen(images, region=PUTTY_WINDOW_HOME)
     except (RuntimeError, TypeError, NameError):
         print("could not find image")
     else:
         pyautogui.click(x, y)
-        if images == 'Speed.PNG':
+        if images == 'home images/Speed.PNG':
             pyautogui.click(x, y)
             pyautogui.press(word_to_list(BAUD_RATE))
-        elif images == 'Serial line.PNG':
+        elif images == 'home images/Serial line.PNG':
             pyautogui.click(x, y)
             pyautogui.press(word_to_list(COM_PORT))
-
 
 e = time.time()
 print(e - s)
